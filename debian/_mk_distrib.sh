da736585227baa65c1432f7113038bdb82ec97bb
ralfoide <ralfoide>
2005-02-16 02:21:26 +0000

    Fixed email in changelog. Adapting mk distrib for SF.

--- debian/_mk_distrib.sh
+++ debian/_mk_distrib.sh
@@ -7,11 +7,16 @@ mkdir -p $TMP
 
 pushd $TMP
 
+# Package name
+# For local cvs: Izumi
+# for SourceForge cvs: izumi
+PACKAGE=izumi
+
 # checkout
-cvs co Izumi
+cvs co $PACKAGE
 
 # Get version number
-A=`grep "([0-9.-]\+)" Izumi/debian/changelog | head -n 1`
+A=`grep "([0-9.-]\+)" $PACKAGE/debian/changelog | head -n 1`
 V1=`echo $A | sed 's/.*(\([0-9\.\]\+\).*/\1/'`
 V2=`echo $A | sed 's/.*(\([0-9\.\-]\+\)).*/\1/'`
 
@@ -19,7 +24,7 @@ PACK="izumi-$V1"
 DEB="izumi_$V2"
 
 # rename folder
-mv Izumi $PACK
+mv $PACKAGE $PACK
 
 # build package
 cd $PACK
